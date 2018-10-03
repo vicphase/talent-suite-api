6faf2f7f9ffdea1dfb539dd044e1c31a3e71de3d
Víctor Martínez <victor@Victors-MacBook-Air.local>
2018-10-02 21:36:19 -0500

    Adding async middleware

--- routes/skills.routes.js
+++ routes/skills.routes.js
@@ -1,37 +1,62 @@
 const express = require('express');
 const Skill = require('../models/skill.model');
 const auth = require('../middleware/auth');
+const asyncMiddleware = require('../middleware/async');
 
 const router = express.Router();
 
-router.get('/', auth, async (req, res) => {
-  const skills = await Skill.find();
-  res.send(skills);
-});
+router.get(
+  '/',
+  auth,
+  asyncMiddleware(async (req, res, next) => {
+    try {
+      const skills = await Skill.find();
+      res.send(skills);
+    } catch (e) {
+      next(e);
+    }
+  })
+);
 
-router.get('/:id', auth, async (req, res) => {
-  const skill = await Skill.findById(req.params.id);
-  res.send(skill);
-});
+router.get(
+  '/:id',
+  auth,
+  asyncMiddleware(async (req, res) => {
+    const skill = await Skill.findById(req.params.id);
+    res.send(skill);
+  })
+);
 
-router.post('/', auth, async (req, res) => {
-  const newSkill = new Skill(req.body);
-  const postSkill = await newSkill.save();
-  res.send(postSkill);
-});
+router.post(
+  '/',
+  auth,
+  asyncMiddleware(async (req, res) => {
+    const newSkill = new Skill(req.body);
+    const postSkill = await newSkill.save();
+    res.send(postSkill);
+  })
+);
 
-router.put('/:id', auth, async (req, res) => {
-  const putSkill = await Skill.findByIdAndUpdate(
-    req.params.id,
-    { $set: req.body },
-    { new: true }
-  );
-  res.send(putSkill);
-});
+router.put(
+  '/:id',
+  auth,
+  asyncMiddleware(async (req, res) => {
+    const putSkill = await Skill.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    res.send(putSkill);
+  })
+);
 
-router.delete('/:id', auth, async (req, res) => {
-  const deleteSkill = await Skill.findByIdAndRemove(req.params.id);
-  res.send(deleteSkill);
-});
+router.delete(
+  '/:id',
+  auth,
+  asyncMiddleware(async (req, res) => {
+    const deleteSkill = await Skill.findByIdAndRemove(req.params.id);
+    res.send(deleteSkill);
+  })
+);
 
 module.exports = router;
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -5,38 +5,51 @@ const admin = require('../middleware/admin');
 const _ = require('lodash');
 const { userRoles } = require('../models/user.model');
 const { User, validate } = require('../models/user.model');
+const asyncMiddleware = require('../middleware/async');
+
 const router = express.Router();
 
-router.get('/', [auth, admin], async (req, res) => {
-  const users = await User.find();
-  res.send(users);
-});
+router.get(
+  '/',
+  [auth, admin],
+  asyncMiddleware(async (req, res) => {
+    const users = await User.find();
+    res.send(users);
+  })
+);
 
-router.get('/me', auth, async (req, res) => {
-  const user = await User.findById(req.user._id).select('-password');
-  res.send(user);
-});
+router.get(
+  '/me',
+  auth,
+  asyncMiddleware(async (req, res) => {
+    const user = await User.findById(req.user._id).select('-password');
+    res.send(user);
+  })
+);
 
-router.post('/', async (req, res) => {
-  const { error } = validate(req.body);
-  if (error) {
-    return res.status(400).send(error.details[0].message);
-  }
-  let user = await User.findOne({ email: req.body.email });
-  if (user) {
-    return res.status(400).send('User already registered.');
-  }
+router.post(
+  '/',
+  asyncMiddleware(async (req, res) => {
+    const { error } = validate(req.body);
+    if (error) {
+      return res.status(400).send(error.details[0].message);
+    }
+    let user = await User.findOne({ email: req.body.email });
+    if (user) {
+      return res.status(400).send('User already registered.');
+    }
 
-  user = new User({ ...req.body, role: userRoles.prospect });
-  const salt = await bcrypt.genSalt(10);
-  user.password = await bcrypt.hash(user.password, salt);
+    user = new User({ ...req.body, role: userRoles.prospect });
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
 
-  await user.save();
+    await user.save();
 
-  const token = user.generateAuthToken();
-  res
-    .header('access-token', token)
-    .send(_.pick(user, ['_id', 'name', 'email', 'skills']));
-});
+    const token = user.generateAuthToken();
+    res
+      .header('access-token', token)
+      .send(_.pick(user, ['_id', 'name', 'email', 'skills']));
+  })
+);
 
 module.exports = router;
