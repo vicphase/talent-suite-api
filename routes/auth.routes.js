7f4b6b22e4925806f48ef03cd06e9bc5df2a73ea
Víctor Martínez <victor@Victors-MacBook-Air.local>
2018-10-02 21:41:10 -0500

    Replacing async middleware

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,31 +3,24 @@ const express = require('express');
 const Joi = require('joi');
 const _ = require('lodash');
 const { User } = require('../models/user.model');
-const asyncMiddleware = require('../middleware/async');
 
 const router = express.Router();
 
-router.post(
-  '/',
-  asyncMiddleware(async (req, res) => {
-    const { error } = validate(req.body);
-    if (error) {
-      return res.status(400).send(error.details[0].message);
-    }
-    let user = await User.findOne({ email: req.body.email });
+router.post('/', async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+  let user = await User.findOne({ email: req.body.email });
 
-    const validPassword = await bcrypt.compare(
-      req.body.password,
-      user.password
-    );
-    if (!validPassword) {
-      return res.status(400).send('Invalid email or password.');
-    }
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) {
+    return res.status(400).send('Invalid email or password.');
+  }
 
-    const token = user.generateAuthToken();
-    res.send(token);
-  })
-);
+  const token = user.generateAuthToken();
+  res.send(token);
+});
 
 function validate(user) {
   const schema = {
